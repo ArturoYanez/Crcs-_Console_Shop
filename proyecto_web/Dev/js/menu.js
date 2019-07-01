b748c6a91049db51d15841829062842f6ca80e95
laika <arturo_w_0@hotmail.com>
2019-07-01 07:32:22 -0400

    Modificacion de la barra de navegacion, adjunto logo, nuevo menu, ypreparacion de pag2

--- proyecto_web/Dev/js/menu.js
+++ proyecto_web/Dev/js/menu.js
@@ -1,6 +1,22 @@
-let btnMenu = document.getElementById('btnmenu');
-let menu = document.getElementById('menu');
-btnMenu.addEventListener('click', function(){
-	'use strict';
-	menu.classList.toggle('mostrar');
-});
+let boton = document.getElementById("icono");
+let enlaces = document.getElementById("enlaces");
+let contador = 0;
+
+boton.addEventListener("click",function(){
+	if(contador == 0){
+		enlaces.className = ('enlaces dos');
+		contador=1;
+	}else{
+		enlaces.classList.remove('dos');
+		enlaces.className = ('enlaces uno');
+		contador = 0;
+	}
+})
+
+window.addEventListener("resize", function(){
+	if(screen.width > 480){
+		contador = 0;
+		enlaces.classList.remove('dos');
+		enlaces.className = ('enlaces uno');
+	}
+})
